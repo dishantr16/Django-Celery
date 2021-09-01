4177dd2506daa523b4b6535d4c6cf582270efab1
dishantr16 <dishant16rathi@gmail.com>
2021-09-01 10:40:10 +0530

    IC

---
+++ celerytask/celery.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import, unicode_literals
+import os
+from celery import Celery
+import celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE','celery_task.settings')
+app = Celery('celery_task')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+app.autodiscover_tasks()
